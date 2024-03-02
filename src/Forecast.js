29347233e0e0c2af06110b457b2dd47f9182a63e
James Gunn <33074318+jag206@users.noreply.github.com>
2024-03-02 21:01:29 +0000

    Render the data dynamically given data

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,37 +1,47 @@
+import React, { useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function Forecast() {
+const Forecast = () => {
+    const [forecast, setForecast] = useState({
+        times: ['9am', '10am', '11am'],
+        temps: [3, 2, 1],
+        rel_humids: [10, 20, 30],
+        abs_humid: [.1, .2, .3],
+    });
+
+    const times = forecast.times.map((time) =>
+        <Col className="text-center">{time}</Col>
+    );
+    const temps = forecast.temps.map((temp) =>
+        <Col className="text-center">{temp} °C</Col>
+    );
+    const rel_humids = forecast.rel_humids.map((rel_humid) =>
+        <Col className="text-center">{rel_humid}%</Col>
+    );
+    const abs_humids = forecast.abs_humid.map((abs_humid) =>
+        <Col className="text-center">{abs_humid}</Col>
+    );
+
     return (
         <Container>
           <Row>
             <Col className="text-center"><strong>Time</strong></Col>
-            <Col className="text-center">9am</Col>
-            <Col className="text-center">10am</Col>
-            <Col className="text-center">11am</Col>
-            <Col className="text-center">12pm</Col>
+            {times}
           </Row>
           <Row>
             <Col className="text-center"><strong>Temperature</strong></Col>
-            <Col className="text-center">10°C</Col>
-            <Col className="text-center">12°C</Col>
-            <Col className="text-center">14°C</Col>
-            <Col className="text-center">16°C</Col>
+            {temps}
           </Row>
           <Row>
             <Col className="text-center"><strong>Rel. Humidity</strong></Col>
-            <Col className="text-center">60%</Col>
-            <Col className="text-center">30%</Col>
-            <Col className="text-center">40%</Col>
-            <Col className="text-center">40%</Col>
+            {rel_humids}
           </Row>
           <Row>
             <Col className="text-center"><strong>Abs. Humidity</strong></Col>
-            <Col className="text-center">3.2</Col>
-            <Col className="text-center">4.1</Col>
-            <Col className="text-center">5.6</Col>
-            <Col className="text-center">2.3</Col>
+            {abs_humids}
           </Row>
         </Container>
     )
