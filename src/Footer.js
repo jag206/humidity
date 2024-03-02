c8114d6c7b1dd57f550d3e860453513fbeba18c9
James Gunn <33074318+jag206@users.noreply.github.com>
2024-03-02 20:46:19 +0000

    Remove unused elements

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,4 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 
 function Footer() {
     return (
