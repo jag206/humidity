28a276d2fbdef03fe11f565a66488d0687982072
James Gunn <33074318+jag206@users.noreply.github.com>
2024-03-04 08:50:52 +0000

    Implement all 3 working sliders

--- src/Widget.js
+++ src/Widget.js
@@ -2,43 +2,63 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 
-const Temperature = () => {
-    const [value, setValue] = useState(10);
-
-    return (
-        <>
-        <Form.Label>Temperature: {Number(value).toFixed(1)} °C</Form.Label>
-        <Form.Range min="-10" max="30" step="0.5" value={value} onChange={e => setValue(e.target.value)}/>
-        </>
-    )
-}
-
-const RelativeHumidity = () => {
-    const [value, setValue] = useState(50);
-    return (
-        <>
-        <Form.Label>Relative Humidity: {value}%</Form.Label>
-        <Form.Range min="0" max="100" step="5" value={value} onChange={e => setValue(e.target.value)}/>
-        </>
-    )
-}
-
-const AbsoluteHumidity = () => {
-    const [value, setValue] = useState(5);
-    return (
-        <>
-        <Form.Label>Absolute Humidity: {Number(value).toFixed(2)}g/kg</Form.Label>
-        <Form.Range min="0" max="20" step="0.5" value={value} onChange={e => setValue(e.target.value)}/>
-        </>
-    )
-}
-
 const Widget = () => {
+    const [temperature, setTemperature] = useState(10);
+    const [relativeHumidity, setRelativeHumidity] = useState(50);
+    const [absoluteHumidity, setAbsoluteHumidity] = useState(10);
+
+    function updateTemperature(e) {
+        setTemperature(e.target.value);
+        calculateAbsoluteHumidity(e.target.value, relativeHumidity);
+    }
+
+    function updateRelativeHumidity(e) {
+        setRelativeHumidity(e.target.value);
+        calculateAbsoluteHumidity(temperature, e.target.value);
+    }
+
+    function updateAbsoluteHumidity(e) {
+        setAbsoluteHumidity(e.target.value);
+        calculateRelativeHumidity(temperature, e.target.value)
+    }
+
+    function calculateAbsoluteHumidity(temperature, relativeHumidity) {
+        // Note: This calculation is completely wrong!
+        setAbsoluteHumidity(temperature * (relativeHumidity / 100));
+    }
+
+    function calculateRelativeHumidity(temperature, absoluteHumidity) {
+        // Note: This calculation is completely wrong!
+        setRelativeHumidity(absoluteHumidity * 100 / temperature);
+    }
+
     return (
     <Container>
-        <Temperature/>
-        <RelativeHumidity/>
-        <AbsoluteHumidity/>
+        <Form.Label>Temperature: {Number(temperature).toFixed(1)} °C</Form.Label>
+        <Form.Range
+            min="-10"
+            max="30"
+            step="0.5"
+            value={temperature}
+            onChange={updateTemperature}
+        />
+        <Form.Label>Relative Humidity: {Number(relativeHumidity).toFixed(0)}%</Form.Label>
+        <Form.Range
+            min="0"
+            max="100"
+            step="5"
+            value={relativeHumidity}
+            onChange={updateRelativeHumidity}
+        />
+        <Form.Label>Absolute Humidity: {Number(absoluteHumidity).toFixed(2)}g/kg</Form.Label>
+        <Form.Range
+            min="0"
+            max="20"
+            step="0.5"
+            value={absoluteHumidity}
+            onChange={updateAbsoluteHumidity}
+        />
+        <p><strong>Note that the above calculation is completely inaccurate</strong></p>
     </Container>
     )
 }
