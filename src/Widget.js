f81101ff203b5afdd401146a19540a95a3f99f9e
James Gunn <33074318+jag206@users.noreply.github.com>
2024-03-05 15:32:38 +0000

    Add lock to humidity calculation

--- src/Widget.js
+++ src/Widget.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 
 const Widget = () => {
+    const [locked, setLocked] = useState("temp");
     const [temperature, setTemperature] = useState(10);
     const [relativeHumidity, setRelativeHumidity] = useState(50);
     const [absoluteHumidity, setAbsoluteHumidity] = useState(10);
@@ -36,6 +37,7 @@ const Widget = () => {
     <Container>
         <Form.Label>Temperature: {Number(temperature).toFixed(1)} °C</Form.Label>
         <Form.Range
+            {...{"disabled":locked==="temp"}}
             min="-10"
             max="30"
             step="0.5"
@@ -44,6 +46,7 @@ const Widget = () => {
         />
         <Form.Label>Relative Humidity: {Number(relativeHumidity).toFixed(0)}%</Form.Label>
         <Form.Range
+            {...{"disabled":locked==="rel_humid"}}
             min="0"
             max="100"
             step="5"
@@ -52,12 +55,43 @@ const Widget = () => {
         />
         <Form.Label>Absolute Humidity: {Number(absoluteHumidity).toFixed(2)}g/kg</Form.Label>
         <Form.Range
+            {...{"disabled":locked==="abs_humid"}}
             min="0"
             max="20"
             step="0.5"
             value={absoluteHumidity}
+
             onChange={updateAbsoluteHumidity}
         />
+        <div key="tmp" className="mb-3">
+          <Form.Check
+            inline
+            {...{"checked":locked==="temp"}}
+            label="Temperature"
+            name="lock"
+            type="radio"
+            id="lock-temp"
+            onChange={e => {setLocked("temp")}}
+          />
+          <Form.Check
+            inline
+            {...{"checked":locked==="rel_humid"}}
+            label="Relative Humidity"
+            name="lock"
+            type="radio"
+            id="lock-rel-humid"
+            onChange={e => {setLocked("rel_humid")}}
+          />
+          <Form.Check
+            inline
+            {...{"checked":locked==="abs_humid"}}
+            label="Absolute Humidity"
+            name="lock"
+            type="radio"
+            id="lock-abs-umid"
+            onChange={e => {setLocked("abs_humid")}}
+          />
+        </div>
         <p><strong>Note that the above calculation is completely inaccurate</strong></p>
     </Container>
     )
